5940f0c1432cc4d44b57027fd667f573d5957478
FASTCHEN <1993480453@qq.com>
2025-05-03 09:56:01 +0800

    Add files via upload

---
+++ Broadcast.cs
@@ -0,0 +1,305 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Oxide.Core;
+using Oxide.Core.Libraries.Covalence;
+using Oxide.Core.Configuration;
+using Oxide.Core.Plugins;
+
+namespace Oxide.Plugins;
+
+[Info("Broadcast", "FastChen", "1.0.0")]
+[Description("A multiple languages supported variables Broadcast and player status.")]
+public class Broadcast : CovalencePlugin
+{
+    private class PluginConfig
+    {
+        [JsonProperty(PropertyName = "Player Options")]
+        public PlayerOptions Player { get; set; } = new();
+
+        [JsonProperty(PropertyName = "Broadcast Options")]
+        public BroadcastOptions Broadcast { get; set; } = new();
+
+        [JsonProperty(PropertyName = "Ping Options")]
+        public PingOptions Ping { get; set; } = new();
+
+        public class PlayerOptions
+        {
+            [JsonProperty(PropertyName = "Show Player Join Message (true/false)")]
+            public bool ShowJoinMessage { get; set; } = true;
+            
+            [JsonProperty(PropertyName = "Show Player Leave Message (true/false)")]
+            public bool ShowLeaveMessage { get; set; } = true;
+        }
+
+        public class BroadcastOptions
+        {
+            [JsonProperty(PropertyName = "Show Prefix (true/false)")]
+            public bool ShowPrefix { get; set; } = true;
+
+            [JsonProperty(PropertyName = "Enable Random Broadcast (true/false)")]
+            public bool EnableRandom { get; set; } = true;
+
+            [JsonProperty(PropertyName = "Random Cycle (Seconds)")]
+            public int RandomCycle { get; set; } = 300;
+        }
+
+        public class PingOptions
+        {
+            [JsonProperty(PropertyName = "Enable High Ping Check (true/false)")]
+            public bool EnableHighPingCheck { get; set; } = true;
+
+            [JsonProperty(PropertyName = "Notification High Ping Player (true/false)")]
+            public bool NotificationHighPingPlayer { get; set; } = true;
+
+            [JsonProperty(PropertyName = "High Ping Limit (Ms)")]
+            public int HighPingLimit { get; set; } = 150;
+
+            [JsonProperty(PropertyName = "Check Cycle (Seconds)")]
+            public int CheckCycle { get; set; } = 300;
+        }
+    }
+
+    private PluginConfig _config;
+
+    void Init()
+    {
+        LoadDefaultConfig();
+        LoadDefaultMessages();
+    }
+
+    protected override void LoadDefaultMessages()
+    {
+        lang.RegisterMessages(new Dictionary<string, string>
+        {
+            {"Prefix", "<color=#68CDFC>[ Broadcast ]</color> "},
+            {"PlayerConnected", "[@PLAYER] 加入!"},
+            {"PlayerDisconnected", "[@PLAYER] 离开!"},
+            {"PlayerHighPing", "[@PLAYER] 当前你的 Ping 值 [@PING](ms) 高于限制: [@PINGLIMIT](ms)!"},
+            
+        }, this, "zh-CN");
+
+        lang.RegisterMessages(new Dictionary<string, string>
+        {
+            {"Prefix", "<color=#68CDFC>[ Broadcast ]</color> "},
+            {"PlayerConnected", "[@PLAYER] Joined!"},
+            {"PlayerDisconnected", "[@PLAYER] Disconnected!"},
+            {"PlayerHighPing", "[@PLAYER] Your Ping [@PING](ms) is above the limit: [@PINGLIMIT](ms)!"}
+        },this, lang.GetServerLanguage());
+    }
+
+    protected override void LoadDefaultConfig()
+    {
+        _config = Config.ReadObject<PluginConfig>();
+
+        if(_config == null)
+        {
+            _config = new PluginConfig();
+        }
+
+        Config.WriteObject(_config, true);
+    }
+
+    private DynamicConfigFile dataFile;
+    private List<string> exampleBroadcastList = new(){ "This is Example Broadcast", "Edit oxide\\data\\Broadcast\\{LangCode}.json" ,"You Can Create More!" };
+
+    void OnServerInitialized()
+    {
+        // Example: lang.GetServerLanguage()(en.json)
+        if(!AutoBroadcastDataFileExists(lang.GetServerLanguage()))
+        {
+            dataFile = Interface.Oxide.DataFileSystem.GetDatafile(Path.Combine("Broadcast", lang.GetServerLanguage()));
+
+            dataFile["BroadcastList"] = exampleBroadcastList;
+            dataFile.Save();
+        }
+        // Example: zh-CN.json
+        // if(!AutoBroadcastDataFileExists("zh-CN"))
+        // {
+        //     dataFile = Interface.Oxide.DataFileSystem.GetDatafile(Path.Combine("Broadcast", "zh-CN"));
+
+        //     dataFile["BroadcastList"] = new List<string>(){ "这是示例 Broadcast", "编辑 oxide\\data\\Broadcast\\zh-CN.json" ,"来添加更多 Broadcast!" };;
+        //     dataFile.Save();
+        // }
+
+        Puts("WORK! WORK!");
+        Puts($"DEBUG: ServerLanguage:{lang.GetServerLanguage()}");
+        PingCheck();
+        AutoBroadcast();
+    }
+
+    
+    [Command("bc")]
+    private void BroadcastCommand(IPlayer player, string command, string[] args)
+    {
+        // if (args.Length < 1)
+        // {
+        //     player.Reply("Test successful!");
+
+        //     player.Reply($"player language:{lang.GetLanguage(player.Id)}");
+        //     return;
+        // }
+
+        string prefix = Lang("Prefix", player.Id);
+
+        foreach(string arg in args)
+        {
+            // if(arg == "send" && args.Length >= 1)
+            // {
+            //     var message = args[1];
+            //     SendMessageToOnlinePlayers(message);
+            // } else
+            if(arg == "ping")
+            {
+                player.Reply($"{prefix}Pong:{player.Ping}(ms)");
+            }
+        }
+
+    }
+
+    void OnUserConnected(IPlayer player)
+    {
+        if(!_config.Player.ShowJoinMessage){ return; }
+
+        string message = Lang("PlayerConnected", player.Id)
+        .Replace("[@PLAYER]", player.Name);
+        
+        Puts(message);
+        SendMessageToOnlinePlayers(message);
+    }
+
+    void OnUserDisconnected(IPlayer player)
+    {
+        if(!_config.Player.ShowLeaveMessage){ return; }
+
+        string message = Lang("PlayerDisconnected", player.Id)
+        .Replace("[@PLAYER]", player.Name);
+        
+        Puts(message);
+        SendMessageToOnlinePlayers(message);
+    }
+
+    void SendMessageToOnlinePlayers(string message)
+    {
+        if (players.Connected.Count() <= 0) return;
+
+        foreach (var player in players.Connected){
+            string prefix = Lang("Prefix", player.Id);
+            player.Message($"{prefix}{message}");
+        }
+    }
+
+
+    void AutoBroadcast()
+    {
+        if(!_config.Broadcast.EnableRandom)
+        {
+            Puts("AutoBroadcast OFF!");
+            return;
+        }
+
+        Puts("AutoBroadcast ON!");
+
+        timer.Every(_config.Broadcast.RandomCycle, () =>
+        {
+            if (players.Connected.Count() <= 0) return;
+
+            DynamicConfigFile broadcastData;
+
+            foreach (var player in players.Connected)
+            {
+                string prefix = "";
+                if(_config.Broadcast.ShowPrefix)
+                {
+                    prefix = Lang("Prefix", player.Id);
+                }
+
+                List<object> broadcastList = null;
+                
+                // 默认读取玩家Lang的通知
+                // Defalut read player Lang datafile.
+                if(AutoBroadcastDataFileExists(lang.GetLanguage(player.Id)))
+                {
+                    broadcastData = Interface.Oxide.DataFileSystem.GetDatafile(Path.Combine("Broadcast", lang.GetLanguage(player.Id)));
+                    broadcastList = broadcastData["BroadcastList"] as List<object>;
+                }
+               
+                // 如果玩家的通知为null或者为0则读取服务器语言的通知
+                // if player lang datafile is null or Count = 0 will get serverlang datafile.
+                if(broadcastList == null || broadcastList.Count == 0)
+                {
+                    broadcastData = Interface.Oxide.DataFileSystem.GetDatafile(Path.Combine("Broadcast", lang.GetServerLanguage()));
+                    broadcastList = broadcastData["BroadcastList"] as List<object>;
+                }
+
+                // 如果还是为空为0就弹出警告到控制台
+                // if serverlang still null or count = 0 will Warning log to console.
+                if(broadcastList == null || broadcastList.Count == 0)
+                {
+                    LogWarning($"No broadcastList found for {player.Name} in {lang.GetLanguage(player.Id)} or {lang.GetServerLanguage()}");
+                    continue;
+                }
+
+                // 发送给玩家
+                // send to player.
+                string message = broadcastList[new System.Random().Next(0, broadcastList.Count)].ToString()
+                .Replace("[@PLAYER]", player.Name)
+                .Replace("[@PING]", player.Ping.ToString())
+                .Replace("[@PINGLIMIT]", _config.Ping.HighPingLimit.ToString())
+                ;
+                
+                player.Message($"{prefix}{message}");
+            }
+        });
+    }
+
+    void PingCheck()
+    {
+        if(!_config.Ping.EnableHighPingCheck)
+        {
+            Puts("PingCheck OFF!");
+            return;
+        }
+
+        Puts("PingCheck ON!");
+        
+        timer.Every(_config.Ping.CheckCycle, () =>
+        {
+            if (players.Connected.Count() <= 0) return;
+
+            foreach (var player in players.Connected)
+            {
+                string prefix = Lang("Prefix", player.Id);
+                string message = Lang("PlayerHighPing", player.Id)
+                .Replace("[@PLAYER]", player.Name)
+                .Replace("[@PING]", player.Ping.ToString())
+                .Replace("[@PINGLIMIT]", _config.Ping.HighPingLimit.ToString())
+                ;
+                if(player.Ping > _config.Ping.HighPingLimit)
+                {
+                    if(_config.Ping.NotificationHighPingPlayer)
+                    {
+                        player.Reply($"{prefix}{message}");
+                    }
+
+                }
+            }
+        });
+    }
+
+    #region Helpers
+
+    T GetConfig<T>(string name, T value) => Config[name] == null ? value : (T)Convert.ChangeType(Config[name], typeof(T));
+
+    string Lang(string key, string id = null, params object[] args) => string.Format(lang.GetMessage(key, this, id), args);
+
+    bool AutoBroadcastDataFileExists(string lang)
+    {
+        return Interface.Oxide.DataFileSystem.ExistsDatafile(Path.Combine("Broadcast", lang));
+    }
+
+    #endregion
+}
---
+++ README.md
@@ -0,0 +1,108 @@
+# Feature
+* **ALL Multi-language support**
+* **ALL Messages Variable Support**
+* Multi-language and Variable support for randomized Broadcast.
+* Player Join or Leave Message.
+* Player High Ping Notification.
+
+# Chat Commands
+
+* `/bc ping` - Check your latency with the server
+
+# Variables
+
+The variables are provided by the plugin.
+You can see in the broadcast file or in the messages language file the difference between using the.
+
+### List of available variables
+
+* `[@PLAYER] ` - Name of the player receiving this broadcast
+  * When a player join/leave the broadcast, the `[@PLAYER] ` is the name of the joining/leaving player.
+* `[@PING]` -  Ping of the player receiving this broadcast (ms)
+* `[@PINGLIMIT]` - Plugin configure `Ping Options > High Ping Limit (Ms)` value
+
+**Variables must be used in the following format:**
+1. All uppercase letters
+2. Add the `@` symbol in front of the letters
+3. Starting with the `[ ` symbol
+4. Ends with the `]` symbol
+
+# Configuration
+## Plugin configure
+Plugin configure path: `\oxide\config\Broadcast.json`
+
+The Default Configuration is Control Function Enablement with Base Settings
+
+```json
+{
+  "Player Options": {
+    "Show Player Join Message (true/false)": true,
+    "Show Player Leave Message (true/false)": true
+  },
+  "Broadcast Options": {
+    "Show Prefix (true/false)": false,
+    "Enable Random Broadcast (true/false)": true,
+    "Random Cycle (Seconds)": 300
+  },
+  "Ping Options": {
+    "Enable High Ping Check (true/false)": true,
+    "Notification High Ping Player (true/false)": true,
+    "High Ping Limit (Ms)": 150,
+    "Check Cycle (Seconds)": 300
+  }
+}
+```
+
+## Broadcast configure
+Broadcast configure Path:`\oxide\data\Broadcast\*.json`
+
+The `Broadcast configure` is used to store Broadcasts in different languages.
+
+### How is work?
+
+> **If multiple language files are not configured, then the server language filesare used by default (en.json is most cases in server language)**
+
+In order to support sending broadcasts to players in different languages,
+you need to configure the contents of (Language_code.json) here
+
+like `\oxide\data\Broadcast\en.json` (en.json is server language in most cases)
+```json
+{
+  "BroadcastList": [
+    "Hello, [@PLAYER] !",
+    "this is a Broadcast.",
+    "This Broadcast came from \\oxide\\data\\Broadcast\\en.json file!"
+  ]
+}
+```
+
+You can create more Language_code.json on `\oxide\data\Broadcast\` folders
+
+like `\oxide\data\Broadcast\zh-CN.json` this wiil be Broadcast to Chinese players.
+```json
+{
+  "BroadcastList": [
+    "你好, [@PLAYER] !",
+    "这是一条广播。",
+    "这条广播来自 \\oxide\\data\\Broadcast\\zh-CN.json 文件中!"
+  ]
+}
+```
+
+or `\oxide\data\Broadcast\ru.json` for Russian players.
+
+```json
+{
+  "BroadcastList": [
+    "Здравствуйте. [@PLAYER] !",
+    "Я не понимаю русский язык."
+  ]
+}
+```
+
+# Localization
+
+you can create or edit lang file to support more content.
+
+
+
